46cdd471fb9e5b82bad063a8b826b78b5bcfe075
Lee-Sunho <sunho9889@naver.com>
2024-07-13 16:59:53 +0900

    [FEAT] #6 로그인 화면 구현

--- app/src/screens/login/Login.tsx
+++ app/src/screens/login/Login.tsx
@@ -1,11 +1,50 @@
-import { GoogleSignin, GoogleSigninButton, GoogleSigninButtonProps } from "@react-native-google-signin/google-signin";
-import appleAuth, {AppleButton, AppleButtonType} from "@invertase/react-native-apple-authentication";
-import * as S from 'screens/login/Login.style'
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import * as S from 'screens/login/Login.style';
+import {useEffect} from 'react';
+import {GOOGLE_WEB_CLIENT_ID} from '@env';
+import GoogleSvg from 'assets/images/GoogleSvg';
+import AppleSvg from 'assets/images/AppleSvg';
+import {Palette} from 'constants/palette';
+import {TouchableOpacity} from 'react-native';
 
 export default function Login() {
-  return <S.Container>
-    <S.Title>{"No more wandering.\nStart your BinVoyage!"}</S.Title>
-    <AppleButton buttonStyle={AppleButton.Style.BLACK} buttonType={AppleButton.Type.CONTINUE} onPress={() => {}}/>
-    <GoogleSigninButton />
-  </S.Container>;
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: GOOGLE_WEB_CLIENT_ID,
+      offlineAccess: false,
+    });
+  });
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.Title>{'No more wandering.\nStart your BinVoyage!'}</S.Title>
+        <S.SignInButtonWrapper>
+          <S.AppleSignInButton>
+            <AppleSvg width="24" height="24" fill={Palette.White} />
+            <S.AppleSignInText>Continue with Apple</S.AppleSignInText>
+          </S.AppleSignInButton>
+          <S.GoogleSignInButton>
+            <GoogleSvg width="25" height="24" />
+            <S.GoogleSignInText>Continue with Google</S.GoogleSignInText>
+          </S.GoogleSignInButton>
+          <S.PassSignInButton>
+            <S.PassSignInText>Continue without logging in</S.PassSignInText>
+          </S.PassSignInButton>
+        </S.SignInButtonWrapper>
+        <S.TextFooterWrapper>
+          <S.TextFooter>By continuing, you agree to our </S.TextFooter>
+          <TouchableOpacity>
+            <S.TextFooterLink>Terms</S.TextFooterLink>
+          </TouchableOpacity>
+        </S.TextFooterWrapper>
+
+        <S.TextFooterWrapper>
+          <S.TextFooter>See how we use your data in our </S.TextFooter>
+          <TouchableOpacity>
+            <S.TextFooterLink>Privacy Policy</S.TextFooterLink>
+          </TouchableOpacity>
+        </S.TextFooterWrapper>
+      </S.Wrapper>
+    </S.Container>
+  );
 }
