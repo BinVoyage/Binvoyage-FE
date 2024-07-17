c135ded7bb5a89e991a6b1519bcaa516f66bb33e
kmin-jeong <desk535353@gmail.com>
2024-07-17 21:00:41 +0900

    [FIX]#5 주석 제거

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -46,25 +46,6 @@ const Map = () => {
   }
 
 
-
-  // function getAddr(lat:any,lng:any){
-  //   // 주소-좌표 변환 객체를 생성합니다,
-  //   let geocoder = new window.kakao.maps.services.Geocoder();
-  
-  //   let coord = new window.kakao.maps.LatLng(lat = location.latitude, lng = location.longitude);
-  //   let callback = function(result:any, status:any) {
-  //       if (status === window.kakao.maps.services.Status.OK) {
-  //           const arr  ={ ...result as Array<adress>};
-  //           const ad = arr[0].road_address;
-  //           const _arr = ad.address_name;
-  //           // region_1depth_name + ad.region_2depth_name + ad.road_name +  ad.main_building_no
-  //           // const _arr = arr[0].road_address.address_name
-  //           console.log(_arr);
-  //       }
-  //   }
-  //   geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-  //   geocoder.coord2RegionCode(coord.getLng(), coord.getLat(), callback);     
-  // }
   
   type Adress = {
     address_name?:string;
