31e68bb9fddf19d577fcd1aa2e48e43be7268cbf
kmin-jeong <desk535353@gmail.com>
2024-07-22 20:14:57 +0900

    [FIX]#5 (주소,거리)가 한쌍으로 콘솔에 찍히게 구현(좌표가 명확함에도 undefined로 찍히는 이슈 해결중)

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -1,4 +1,4 @@
-import  { MutableRefObject, useEffect, useMemo, useRef } from "react";
+import  { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
 import CurrentTab from "./CurrentTab";
 import Filter from "./Filter";
 import { trashpositions,recyclepositions } from "./Places";
@@ -24,6 +24,7 @@ type CurrentLocation = {
 };
 
 const Map = ({ latitude, longitude }: CurrentLocation) => {
+  const [ads, setAd] = useState<any | string>('');
   const mapRef = useRef<HTMLElement | null>(null);
   const initMap = () =>{
     // if (typeof location != 'string' ){
@@ -51,13 +52,38 @@ const Map = ({ latitude, longitude }: CurrentLocation) => {
         image: currentImage,
       });
 
-    
+      
+
+
    // 쓰레기 마커 
    let trashimageSrc = "src/assets/trashmark.svg";
 
    const settrashMarkers = (maps: any) => {
-    trashpositions.forEach((obj) => {
+    trashpositions.forEach(async (obj) => {
       const trashLocation = obj;
+
+      const p = await printAddr();
+
+      function printAddr(): Promise<string | undefined | null> {
+        return new Promise((resolve, reject) => {
+          let geocoder = new window.kakao.maps.services.Geocoder();
+          let coord = obj;
+          
+          let callback = function(result: Array<any>, status: any) {
+            if (status === window.kakao.maps.services.Status.OK) {
+              const ad = result[0]?.road_address;
+              const _arr = ad?.address_name;
+              // setAd(_arr)
+              resolve(_arr); 
+            } else {
+              reject("Failed to get address"); 
+            }
+            // return (ads)
+          }
+          
+          geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+        });
+      }
 
       new window.kakao.maps.Marker({
         map: maps,
@@ -70,8 +96,8 @@ const Map = ({ latitude, longitude }: CurrentLocation) => {
       });
   
       const trashdistance = poly.getLength();
-      if ( trashdistance < 2000) {
-        console.log(trashLocation,"그냥 쓰레기통 거리:", trashdistance)
+      if (trashdistance < 2000) {
+        console.log("그냥 :",p,"그냥 쓰레기통 거리:", trashdistance)
       }
     })
   }
@@ -81,11 +107,41 @@ const currentCenter = new window.kakao.maps.LatLng(
   latitude, longitude
 );
 
+
+
+// 재활용 쓰레기통
+
 let recycleimageSrc = "src/assets/recyclemark.svg";
 
+
 const setrecycleMarkers = (maps: any) => {
-  recyclepositions.forEach((obj) => {
+  recyclepositions.forEach(async (obj) => {
     const binLocation = obj;
+
+    const r = await printAddr();
+
+    function printAddr(): Promise<string | undefined | null> {
+      return new Promise((resolve, reject) => {
+        let geocoder = new window.kakao.maps.services.Geocoder();
+        let coord = obj;
+        
+        let callback = function(result: Array<any>, status: any) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const ad = result[0]?.road_address;
+            const _arr = ad?.address_name;
+            // setAd(_arr)
+            resolve(_arr); 
+          } else {
+            reject("Failed to get address"); 
+          }
+          // return (ads)
+        }
+        
+        geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+      });
+    }
+
+
 
     new window.kakao.maps.Marker({
       map: maps,
@@ -99,7 +155,7 @@ const setrecycleMarkers = (maps: any) => {
 
     const recycledistance = poly.getLength();
     if (recycledistance < 2000) {
-      console.log(binLocation,"재활용 쓰레기통 거리:",recycledistance)
+      console.log("재활용 :",r,",",recycledistance)
     }
 
   })
