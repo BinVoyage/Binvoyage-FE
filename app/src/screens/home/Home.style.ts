87f828e1fd82b955d1c99dbe25d9a07af44ea876
Lee-Sunho <sunho9889@naver.com>
2024-07-23 17:56:46 +0900

    [FEAT] #12 홈 화면 퍼블리싱 중

--- app/src/screens/home/Home.style.ts
+++ app/src/screens/home/Home.style.ts
@@ -5,19 +5,20 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: ${Palette.Primary};
-  padding: 18px 16px 0px;
+  padding-top: 18px;
 `;
 
 export const HeaderTitle = styled.Text`
   font-size: ${Typo.Title2.fontSize};
   font-weight: ${Typo.Title2.fontWeight};
   color: ${Palette.White};
+  padding: 0px 16px;
 `;
 
 export const Bridge = styled.TouchableOpacity`
   flex-direction: row;
   height: 92px;
-  margin: 12px 0px 26px;
+  margin: 12px 16px 26px;
 `;
 
 export const BridgeIconWrapper = styled.ImageBackground`
@@ -27,16 +28,73 @@ export const BridgeIconWrapper = styled.ImageBackground`
   justify-content: center;
 `;
 
-export const BridgeTextWrapper = styled.View`
+export const BridgeTextWrapper = styled.ImageBackground`
   flex: 1;
   height: 92px;
   justify-content: center;
   padding-left: 12px;
-  background: ${Palette.White};
 `;
 
 export const BridgeText = styled.Text`
   font-size: ${Typo.Button1.fontSize};
   font-weight: ${Typo.Button1.fontWeight};
   color: ${Palette.Black};
+`;
+
+export const Body = styled.View`
+  flex: 1;
+  border-radius: 12px 12px 0px 0px;
+  background: ${Palette.Gray1};
+  padding: 22px 20px 31px;
+`;
+
+export const BodyTitle = styled.Text`
+  font-size: ${Typo.Title1.fontSize};
+  font-weight: ${Typo.Title1.fontWeight};
+  color: ${Palette.Black};
+`;
+
+export const BodyDescription = styled.Text`
+  font-size: ${Typo.B1.fontSize};
+  font-weight: ${Typo.B1.fontWeight};
+  color: ${Palette.Gray6};
+  margin: 6px 0px 22px;
+`;
+
+export const PassPortBg = styled.View`
+  position: relative;
+  height: 363px;
+  background: ${Palette.White};
+  border-radius: 0px 18px 18px 0px;
+`
+
+export const PassPort = styled.View`
+  width: 83%;
+  height: 363px;
+  background: ${Palette.Secondary};
+  border-radius: 0px 18px 18px 0px;
+`
+export const ArrowNextWrapper = styled.TouchableOpacity`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 50%;
+  right: 13px;
+  margin-top: -12px;
+`
+
+export const Button = styled.TouchableOpacity`
+  width: 100%;
+  padding: 16px 30px;
+  justify-content: center;
+  align-items: center;
+  background: ${Palette.Primary};
+  border-radius: 10px;
+  margin-top: auto;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: ${Typo.Button1.fontSize};
+  font-weight: ${Typo.Button1.fontWeight};
+  color: ${Palette.White};
 `;
--- app/src/screens/home/Home.tsx
+++ app/src/screens/home/Home.tsx
@@ -1,5 +1,6 @@
+import ArrowNextSvg from 'assets/images/ArrowNextSvg';
 import HomeTrashSvg from 'assets/images/HomeTrashSvg';
-import {Text, View} from 'react-native';
+import { Palette } from 'constants/palette';
 import * as S from 'screens/home/Home.style';
 
 export default function Home() {
@@ -7,13 +8,26 @@ export default function Home() {
     <S.Container>
       <S.HeaderTitle>Julia, Start your BinVoyage!</S.HeaderTitle>
       <S.Bridge>
-        <S.BridgeIconWrapper source={require('assets/images/HomeIconWrapperSvg')}>
+        <S.BridgeIconWrapper source={require('assets/images/icon-trash-wrapper.png')} resizeMode='cover'>
           <HomeTrashSvg width="40" height="66" />
         </S.BridgeIconWrapper>
-        <S.BridgeTextWrapper>
-          <S.BridgeText>Struggling to find bins? 😓 There’s one just 350m away!</S.BridgeText>
+        <S.BridgeTextWrapper source={require('assets/images/icon-text-wrapper.png')} resizeMode='stretch'>
+          <S.BridgeText>{`Struggling to find bins? 😓\nThere’s one just 350m away!`}</S.BridgeText>
         </S.BridgeTextWrapper>
       </S.Bridge>
+      <S.Body>
+        <S.BodyTitle>{`Collect Seoul\nstamps during BinVoyage`}</S.BodyTitle>
+        <S.BodyDescription>Stamp varies by the bin location.</S.BodyDescription>
+        <S.PassPortBg>
+        <S.ArrowNextWrapper>
+            <ArrowNextSvg width='9' height='16' fill={Palette.Gray4}/>
+          </S.ArrowNextWrapper>
+          <S.PassPort></S.PassPort>
+        </S.PassPortBg>
+        <S.Button>
+          <S.ButtonText>Find any new bin? Let us know!</S.ButtonText>
+        </S.Button>
+      </S.Body>
     </S.Container>
   );
 }
