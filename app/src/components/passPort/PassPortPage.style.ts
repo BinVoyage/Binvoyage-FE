e10f87a7c72d934cf9d6d1a33edd36f2dff6a0e1
Lee-Sunho <sunho9889@naver.com>
2024-07-26 23:31:20 +0900

    [FEAT] #12 스탬프 화면 구현

--- app/src/components/passPort/PassPortPage.style.ts
+++ app/src/components/passPort/PassPortPage.style.ts
@@ -1,22 +1,24 @@
 import {Palette} from 'constants/palette';
 import styled from 'styled-components/native';
-import { Dimensions } from 'react-native';
+import {Dimensions} from 'react-native';
 
 const {width, height} = Dimensions.get('window');
 
 export const Container = styled.View`
+  position: relative;
   flex: 1;
   background: ${Palette.Secondary};
   border-radius: 12px;
-  padding: 20px;
-  flex-direction: row;
+  /* flex-direction: row;
   justify-content: space-between;
-  flex-wrap: wrap;
-  gap: 20px;
+  flex-wrap: wrap; */
 `;
 
-export const StampWrapper = styled.ImageBackground`
-  flex: 0 0 ${(width - 92) / 2}px;
-  height: ${(height - 270) / 3}px;
-  border: 1px solid red;
-`
+export const StampWrapper = styled.ImageBackground<{top: number; left: number; zIndex: number}>`
+  position: absolute;
+  width: ${(width - 32) / 2}px;
+  height: ${(height - 190) / 3}px;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
+  z-index: ${props => props.zIndex};
+`;
