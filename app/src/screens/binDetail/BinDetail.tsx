011e9c9966cf74c4d32ec3a8bb2cd91b61a3aed6
Lee-Sunho <sunho9889@naver.com>
2024-08-10 15:59:56 +0900

    [FEAT] #24 길찾기: 지도앱 연결 구현

--- app/src/screens/binDetail/BinDetail.tsx
+++ app/src/screens/binDetail/BinDetail.tsx
@@ -4,10 +4,11 @@ import ArrowDownSvg from 'assets/images/ArrowDownSvg';
 import ArrowNextSvg from 'assets/images/ArrowNextSvg';
 import BinSvg from 'assets/images/BinSvg';
 import FootPrintSvg from 'assets/images/FootPrintSvg';
+import ModalOpenMap from 'components/modalOpenMap/ModalOpenMap';
 import ReviewItem from 'components/reviewItem/ReviewItem';
 import {Palette} from 'constants/palette';
 import {useEffect, useState} from 'react';
-import {ScrollView, Text, TouchableOpacity, View, Linking, Platform} from 'react-native';
+import {ScrollView, Text, TouchableOpacity, View, Linking, Platform, Alert} from 'react-native';
 import * as S from 'screens/binDetail/BinDetail.style';
 import {formatDate} from 'utils/formatDate';
 
@@ -20,6 +21,7 @@ export default function BinDetail({route}: BinDetailProps) {
   const navigation = useNavigation<NavigationProp<RootBinDetailParamList>>();
   const [binData, setBinData] = useState<BinDetail | null>(null);
   const [feedbackList, setFeedbackList] = useState<Feedback[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const getBinData = async () => {
@@ -46,127 +48,116 @@ export default function BinDetail({route}: BinDetailProps) {
     getFeedbackData();
   }, []);
 
-  const openMaps = () => {
-    const [latitude, longitude] = binData!.coordinate;
-    const label = 'Destination'; // 목적지의 레이블
-
-    const url = Platform.select({
-      ios: `maps:0,0?q=${label}@${latitude},${longitude}`,
-      android: `geo:0,0?q=${latitude},${longitude}(${label})`,
-    });
-
-    if (url) {
-      Linking.openURL(url).catch(err => console.error('Error opening map:', err));
-    }
-  };
-
   return (
-    <S.Container>
-      <S.ArrowDownWrapper onPress={() => navigation.goBack()}>
-        <ArrowDownSvg width="24" height="24" fill={Palette.Gray4} />
-      </S.ArrowDownWrapper>
-      <ScrollView bounces={false} style={{flex: 1, backgroundColor: Palette.Gray1}}>
-        <S.TopContainer>
-          <S.TopLabel>
-            <S.TopLabelText>Most users found this bin!</S.TopLabelText>
-          </S.TopLabel>
-          <S.Title>{binData?.address}</S.Title>
-          <S.RowWrapper style={{justifyContent: 'space-between'}}>
-            <S.RowWrapper>
+    <>
+      <S.Container>
+        <S.ArrowDownWrapper onPress={() => navigation.goBack()}>
+          <ArrowDownSvg width="24" height="24" fill={Palette.Gray4} />
+        </S.ArrowDownWrapper>
+        <ScrollView bounces={false} style={{flex: 1, backgroundColor: Palette.Gray1}}>
+          <S.TopContainer>
+            <S.TopLabel>
+              <S.TopLabelText>Most users found this bin!</S.TopLabelText>
+            </S.TopLabel>
+            <S.Title>{binData?.address}</S.Title>
+            <S.RowWrapper style={{justifyContent: 'space-between'}}>
               <S.RowWrapper>
-                <BinSvg width="18" height="18" fill={Palette.Gray4} />
-                <S.TextInfo1>{binData?.type_name}</S.TextInfo1>
+                <S.RowWrapper>
+                  <BinSvg width="18" height="18" fill={Palette.Gray4} />
+                  <S.TextInfo1>{binData?.type_name}</S.TextInfo1>
+                </S.RowWrapper>
+                <S.Division />
+                <S.RowWrapper>
+                  <FootPrintSvg width="24" height="24" fill={Palette.Gray4} />
+                  <S.TextInfo1>100m</S.TextInfo1>
+                </S.RowWrapper>
               </S.RowWrapper>
-              <S.Division />
-              <S.RowWrapper>
-                <FootPrintSvg width="24" height="24" fill={Palette.Gray4} />
-                <S.TextInfo1>100m</S.TextInfo1>
-              </S.RowWrapper>
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate('ReportWrongInfo', {
+                    bin_id: binData?.bin_id ?? -1,
+                    type_name: binData?.type_name ?? '',
+                    location_type_name: binData?.location_type_name ?? '',
+                    address: binData?.address ?? '',
+                    detail: binData?.detail ?? '',
+                    image: binData?.image ?? '',
+                  })
+                }>
+                <S.RowWrapper>
+                  <S.TextWrongInfo>Wrong Info?</S.TextWrongInfo>
+                  <ArrowNextSvg width="14" height="14" fill={Palette.Primary} />
+                </S.RowWrapper>
+              </TouchableOpacity>
             </S.RowWrapper>
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate('ReportWrongInfo', {
-                  bin_id: binData?.bin_id ?? -1,
-                  type_name: binData?.type_name ?? '',
-                  location_type_name: binData?.location_type_name ?? '',
-                  address: binData?.address ?? '',
-                  detail: binData?.detail ?? '',
-                  image: binData?.image ?? '',
-                })
-              }>
+            <S.ImageArea />
+          </S.TopContainer>
+          <S.GrayContainer>
+            <S.DetailWrapper>
               <S.RowWrapper>
-                <S.TextWrongInfo>Wrong Info?</S.TextWrongInfo>
-                <ArrowNextSvg width="14" height="14" fill={Palette.Primary} />
+                <S.TextLocationPrimary>Location details</S.TextLocationPrimary>
+                <S.LabelLocation>
+                  <S.LabelLocationText>{binData?.location_type_name}</S.LabelLocationText>
+                </S.LabelLocation>
               </S.RowWrapper>
-            </TouchableOpacity>
-          </S.RowWrapper>
-          <S.ImageArea />
-        </S.TopContainer>
-        <S.GrayContainer>
-          <S.DetailWrapper>
-            <S.RowWrapper>
-              <S.TextLocationPrimary>Location details</S.TextLocationPrimary>
-              <S.LabelLocation>
-                <S.LabelLocationText>{binData?.location_type_name}</S.LabelLocationText>
-              </S.LabelLocation>
-            </S.RowWrapper>
-            <S.TextLocationContents>{binData?.detail}</S.TextLocationContents>
-          </S.DetailWrapper>
-          <S.DetailWrapper>
-            <S.DetailTitle>Most users found this bin last month!</S.DetailTitle>
-            <S.VisitRecordWrapper>
-              <S.VisitRecordBox>
-                <S.TextVisitNum isSuccessful>{binData?.success_count}</S.TextVisitNum>
-                <S.VisitDescription>Successful visits</S.VisitDescription>
-              </S.VisitRecordBox>
-              <S.VisitRecordBox>
-                <S.TextVisitNum>{binData?.fail_count}</S.TextVisitNum>
-                <S.VisitDescription>Failed visits</S.VisitDescription>
-              </S.VisitRecordBox>
-            </S.VisitRecordWrapper>
-            {/* 최근 n개의 발견 / 실패 리스트 출력 */}
-            {binData?.visit_list
-              ? binData.visit_list.slice(0, 5).map((item, index) => (
-                  <S.RowWrapper style={{marginBottom: 2}} key={index}>
-                    <S.TextDate>{formatDate(item.visit_dt)}</S.TextDate>
-                    <S.VisitDescription>
-                      Someone <Text style={{color: item.is_success ? Palette.Primary : Palette.Secondary2}}>found</Text> this bin
-                    </S.VisitDescription>
-                  </S.RowWrapper>
-                ))
-              : null}
-          </S.DetailWrapper>
-          <S.DetailWrapper style={{paddingBottom: 0}}>
-            <S.DetailTitle>
-              After-visit feedback <Text style={{color: Palette.Primary}}>{feedbackList.length}</Text>
-            </S.DetailTitle>
-            <View style={{marginTop: 18, marginBottom: 30}}>
-              {feedbackList.map((item, index) => (
-                <ReviewItem
-                  key={item.feedback_id}
-                  feedbackId={item.feedback_id}
-                  date={formatDate(item.registration_dt)}
-                  author={item.user_name}
-                  content={item.content}
-                  isLast={index === feedbackList.length - 1 ? true : false}
-                />
-              ))}
-            </View>
-            <S.SeeAllWrapper onPress={() => navigation.navigate('FeedbackList')}>
-              <S.TextSeeAll>See All</S.TextSeeAll>
-              <ArrowDownSvg width="24" height="24" fill={Palette.Gray4} />
-            </S.SeeAllWrapper>
-          </S.DetailWrapper>
-        </S.GrayContainer>
-      </ScrollView>
-      <S.BtnContainer>
-        <S.Button isPrimary>
-          <S.ButtonText>Verify visit</S.ButtonText>
-        </S.Button>
-        <S.Button onPress={openMaps}>
-          <S.ButtonText>Get directions</S.ButtonText>
-        </S.Button>
-      </S.BtnContainer>
-    </S.Container>
+              <S.TextLocationContents>{binData?.detail}</S.TextLocationContents>
+            </S.DetailWrapper>
+            <S.DetailWrapper>
+              <S.DetailTitle>Most users found this bin last month!</S.DetailTitle>
+              <S.VisitRecordWrapper>
+                <S.VisitRecordBox>
+                  <S.TextVisitNum isSuccessful>{binData?.success_count}</S.TextVisitNum>
+                  <S.VisitDescription>Successful visits</S.VisitDescription>
+                </S.VisitRecordBox>
+                <S.VisitRecordBox>
+                  <S.TextVisitNum>{binData?.fail_count}</S.TextVisitNum>
+                  <S.VisitDescription>Failed visits</S.VisitDescription>
+                </S.VisitRecordBox>
+              </S.VisitRecordWrapper>
+              {/* 최근 n개의 발견 / 실패 리스트 출력 */}
+              {binData?.visit_list
+                ? binData.visit_list.slice(0, 5).map((item, index) => (
+                    <S.RowWrapper style={{marginBottom: 2}} key={index}>
+                      <S.TextDate>{formatDate(item.visit_dt)}</S.TextDate>
+                      <S.VisitDescription>
+                        Someone <Text style={{color: item.is_success ? Palette.Primary : Palette.Secondary2}}>found</Text> this bin
+                      </S.VisitDescription>
+                    </S.RowWrapper>
+                  ))
+                : null}
+            </S.DetailWrapper>
+            <S.DetailWrapper style={{paddingBottom: 0}}>
+              <S.DetailTitle>
+                After-visit feedback <Text style={{color: Palette.Primary}}>{feedbackList.length}</Text>
+              </S.DetailTitle>
+              <View style={{marginTop: 18, marginBottom: 30}}>
+                {feedbackList.map((item, index) => (
+                  <ReviewItem
+                    key={item.feedback_id}
+                    feedbackId={item.feedback_id}
+                    date={formatDate(item.registration_dt)}
+                    author={item.user_name}
+                    content={item.content}
+                    isLast={index === feedbackList.length - 1 ? true : false}
+                  />
+                ))}
+              </View>
+              <S.SeeAllWrapper onPress={() => navigation.navigate('FeedbackList')}>
+                <S.TextSeeAll>See All</S.TextSeeAll>
+                <ArrowDownSvg width="24" height="24" fill={Palette.Gray4} />
+              </S.SeeAllWrapper>
+            </S.DetailWrapper>
+          </S.GrayContainer>
+        </ScrollView>
+        <S.BtnContainer>
+          <S.Button isPrimary>
+            <S.ButtonText>Verify visit</S.ButtonText>
+          </S.Button>
+          <S.Button onPress={() => setIsModalOpen(true)}>
+            <S.ButtonText>Get directions</S.ButtonText>
+          </S.Button>
+        </S.BtnContainer>
+      </S.Container>
+      {isModalOpen ? <ModalOpenMap setIsModalOpen={setIsModalOpen} coords={binData?.coordinate} /> : null}
+    </>
   );
 }
