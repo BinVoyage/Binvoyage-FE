45d494c45d92584241cc5309a3da7aff57c10650
Lee-Sunho <sunho9889@naver.com>
2024-08-17 00:25:46 +0900

    [FIX] 안드로이드 패키지명 변경

--- app/android/app/src/main/java/com/binvoyage/MainActivity.kt
+++ app/android/app/src/main/java/com/binvoyage/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app
+package com.binvoyage
 
 import android.os.Bundle
 import com.facebook.react.ReactActivity
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "app"
+  override fun getMainComponentName(): String = "binvoyage"
 
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
