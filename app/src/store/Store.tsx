abdd26c74e42954c4db65ae2455e5f2288d42929
Lee-Sunho <sunho9889@naver.com>
2024-08-17 18:18:02 +0900

    [REFACTOR] 지도 렌더링 성능 개선 및 코드 리팩토링

--- app/src/store/Store.tsx
+++ app/src/store/Store.tsx
@@ -1,8 +1,13 @@
+import Geolocation from '@react-native-community/geolocation';
 import {create} from 'zustand';
 
 interface mapStore {
   currentPosition: CurrentPosition | null;
   setCurrentPosition: (position: CurrentPosition) => void;
+  watcherId: number | null;
+  setWatcherId: (id: number | null) => void;
+  startWatchingPosition: (onPositionUpdate?: (position: CurrentPosition) => void) => void;
+  stopWatchingPosition: () => void;
 }
 
 interface userStore {
@@ -15,9 +20,37 @@ interface Picturestore {
   addImages: (newImage: Image) => void;
 }
 
-export const mapStore = create<mapStore>(set => ({
+export const mapStore = create<mapStore>((set, get) => ({
   currentPosition: null,
+  watcherId: null,
+  setWatcherId: id => set({watcherId: id}),
   setCurrentPosition: position => set({currentPosition: position}),
+  startWatchingPosition: onPositionUpdate => {
+    const watcherId = Geolocation.watchPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        set({currentPosition: {latitude, longitude}});
+
+        // 위치가 업데이트될 때마다 WebView로 메시지 전송
+        if (onPositionUpdate) {
+          onPositionUpdate({latitude: latitude, longitude: longitude});
+        }
+      },
+      error => {
+        console.log('Geolocation error:', error.code, error.message);
+      },
+      {enableHighAccuracy: false, timeout: 20000, maximumAge: 10000, distanceFilter: 10},
+    );
+    set({watcherId});
+  },
+
+  stopWatchingPosition: () => {
+    const {watcherId} = get();
+    if (watcherId !== null) {
+      Geolocation.clearWatch(watcherId);
+      set({watcherId: null});
+    }
+  },
 }));
 
 export const userStore = create<userStore>(set => ({
