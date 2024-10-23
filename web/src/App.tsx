28b297111bb7682a066a800ba0606de03ebcafeb
Lee-Sunho <sunho9889@naver.com>
2024-10-23 20:23:45 +0900

    deploy

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -145,8 +145,9 @@ function App() {
         <Route
           path="/reportNewBin"
           element={
+            isLocationSet && currentLocation?
             <ReportNewBin latitude={locationToUse.latitude}
-            longitude={locationToUse.longitude} triggerRefresh={triggerRefresh} />
+            longitude={locationToUse.longitude} triggerRefresh={triggerRefresh} /> : null
           }
         />
       </Routes>
