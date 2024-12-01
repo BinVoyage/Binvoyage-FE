00c8d72c6d73be7910be938d265cb4b8d6673fd2
Lee-Sunho <sunho9889@naver.com>
2024-12-01 16:41:19 +0900

    [FEAT] #38 스탬프 모달 lottie 적용

--- app/src/components/modalVerifyVisit/ModalStamp.tsx
+++ app/src/components/modalVerifyVisit/ModalStamp.tsx
@@ -1,15 +1,26 @@
 import {NavigationProp, useNavigation} from '@react-navigation/native';
 import * as S from 'components/modalVerifyVisit/ModalVerifyVisit.style';
 import {Image} from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function ModalStamp() {
   const navigation = useNavigation<NavigationProp<RootTabParamList>>();
+  let animation;
   return (
     <S.Background>
       <S.Container>
         <S.Title>Thanks for your BinVoyage!</S.Title>
         <S.TextAddress>New stamp added!</S.TextAddress>
-        <Image source={require('assets/images/img-verify-stamp.png')} style={{width: 181, height: 176}} />
+        <LottieView
+          // ref={animation => {
+          //   this.animation = animation;
+          // }}
+          source={require('assets/images/stamp/stamp_lottie.json')} // 애니메이션 파일 경로
+          autoPlay
+          loop={false}
+          style={{width: 181, height: 176}}
+        />
+        {/* <Image source={require('assets/images/img-verify-stamp.png')} style={{width: 181, height: 176}} /> */}
         <S.TextB1>Check your passport in HOME</S.TextB1>
         <S.Button isPrimary onPress={() => navigation.navigate('HomeNavigator', {screen: 'Home'})}>
           <S.ButtonText isPrimary>Okay!</S.ButtonText>
